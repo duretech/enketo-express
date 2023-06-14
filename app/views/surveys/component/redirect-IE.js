f5eb69feb5392adc5166987f7b2fac926a3226f7
samirAtdure <samir@duretechnologies.com>
2023-06-14 15:04:57 +0530

    removing files.

--- app/views/surveys/component/redirect-IE.js
+++
@@ -1,3 +0,0 @@
-if (navigator.userAgent.indexOf('Trident/') >= 0) {
-    window.location.href = '/modern-browsers';
-}
--- scripts/build.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env node */
-
-const esbuild = require('esbuild');
-const config = require('../config/build.js');
-
-esbuild.build(config);
